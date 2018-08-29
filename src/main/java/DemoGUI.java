fe8d319c48eb059106e4e02e0252c9ac3e61dd3d
MINGFENG LI <merlinlee5513@gmail.com>
2018-08-30 09:30:11 +1000

    Second Commit: BUILD PROJECT BY Gradle

---
+++ src/main/java/DemoGUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DemoGUI {
+    public DemoGUI() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("DemoGUI");
+        frame.setContentPane(new DemoGUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        Server server = Server.getServerInstance();
+        frame.setVisible(true);
+    }
+
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+}
--- src/main/java/ServerGUI.java
+++
@@ -1,8 +0,0 @@
-/**
- * GUI for Server
- *
- * @author MINGFENG LI
- * @date 30/08/2018
- */
-public class ServerGUI {
-}
