fe05a4bcaf790bb9e3911bcfe5bd28c5ac9e448a
MINGFENG LI <merlinlee5513@gmail.com>
2018-09-06 17:11:55 +1000

    Third Commit: FINAL RELEASE

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -4,8 +4,7 @@ import org.apache.log4j.PropertyConfigurator;
 
 public class ClientMain {
     public static void main(String[] args){
-        ClientMain clientMain = new ClientMain();
-        PropertyConfigurator.configure(ClientMain.class.getResourceAsStream("/log4j.properties"));
+        PropertyConfigurator.configure(ClientMain.class.getResourceAsStream("/log4j_1.properties"));
         ClientController clientController = ClientController.getServerInstance(Integer.parseInt(args[0]),args[1]);
         clientController.createUI();
 
---
+++ src/main/java/server/UI/ServerGUI.java
@@ -0,0 +1,11 @@
+package server.UI;
+
+import javax.swing.*;
+
+public class ServerGUI {
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
